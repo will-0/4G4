b8d892a6fc9b072badfb250a4ec707db35843b4a
Kim Barker <kim-barker@sjc187n114.joh.private.cam.ac.uk>
2020-03-04 00:31:56 +0000

    Changes to POD-NN, add accuracy to class, further experimentation

--- Code/SwarmParty.py
+++ Code/SwarmParty.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pylab as plt
+import tensorflow as tf
+from tensorflow import keras
 
 class NN_Swarm():
     
@@ -26,11 +28,12 @@ class NN_Swarm():
         self.p_best = self.current_pos
         self.v = self.v_min + (self.v_max-self.v_min)*np.random.rand(self.n_particles, self.n_dims)
     
-    def provide_data(self, X_train, X_test, Y_train, Y_test):
+    def provide_data(self, X_train, X_test, Y_train, Y_test, names = None):
         self.X_train = X_train
         self.X_test = X_test
         self.Y_train = Y_train
         self.Y_test = Y_test
+        self.names = names
         print(self.current_pos.shape)
         try:
             float(self.model.evaluate(self.X_test, self.Y_test, verbose=0))
@@ -42,14 +45,14 @@ class NN_Swarm():
         self.gbest_perform = self.f(self.g_best)
         self.nn_weights = self._ConvertBack(self.g_best)      
 
-    def train(self, iterations = 30, give_curve = False, train_fast = False):
+    def train(self, iterations = 30, give_curve = False, train_fast = False, patience = False):
         """ Function to initiate swarm optimization of the weights and biases for the keras
         model provided. Runs for 50 iterations by default. Set give_curve = True to directly
         return a training curve (a numpy array with performance evaluations on training and 
         test data for each iteration) """
 
         try:
-            self.model
+            self.model.set_weights(self._ConvertBack(self.g_best))
             self.X_train
             self.X_test
             self.Y_train
@@ -66,7 +69,9 @@ class NN_Swarm():
         is_better = np.zeros([self.n_particles,1])
         
         for i in range(iterations):
-            m_v_max = self.v_max #*g_best_perform
+            
+            ## PSO Loop
+            m_v_max = self.v_max*np.exp(i/iterations) #*g_best_perform
 
             #update the positions using the velocity
             self.v += self.c_1*(np.random.rand(self.current_pos.shape[0],1))*(self.p_best-self.current_pos)
@@ -75,18 +80,27 @@ class NN_Swarm():
             self.v = np.where(v_norm < m_v_max, self.v, m_v_max*self.v/v_norm)                       #(comment in to use it)
             self.current_pos += self.v
             curr_perform = self.f(self.current_pos)
+            
             #replace the p_bests with the current location if they're better
             is_better = (curr_perform<self.pbest_perform).reshape([is_better.shape[0],1])
             self.p_best = is_better*self.current_pos + np.logical_not(is_better)*self.p_best
             self.pbest_perform = is_better*curr_perform + np.logical_not(is_better)*self.pbest_perform
+            
             #update g_best
             if np.min(self.pbest_perform) < self.gbest_perform:
                 self.g_best = self.p_best[np.argmin(self.pbest_perform),:]
                 self.gbest_perform = np.min(self.pbest_perform)
+                count_no_improv = 0
+            elif patience != False:
+                count_no_improv += 1
+                if count_no_improv > patience:
+                    break
             print("\rPerformance at iteration " + str(i+1) + ": " + str(self.gbest_perform), end = "\r")
+            
             if not train_fast:
                 training_curve.append([self.gbest_perform.astype(float), self.f(self.g_best, val_set = True)[0][0].astype(float)])
         self.nn_weights = self._ConvertBack(self.g_best) 
+        
         if not train_fast:
             self.train_curve = np.array(training_curve)
             if give_curve:
@@ -104,17 +118,99 @@ class NN_Swarm():
             else:
                 objective[part_ind] = self.model.evaluate(self.X_test, self.Y_test, verbose=0)
         return objective
+    
+    def get_accuracy(self, give_curve = False, isMnist = False):
+        
+        if isMnist == True:
+            # set weights and predict with model
+            probability_model = keras.Sequential([self.model, keras.layers.Softmax()])
+            predictions = probability_model.predict(self.X_test)
 
-    def plot_training(self):
+            def plot_image(i, predictions_array, true_label, img):
+                predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+                plt.grid(False)
+                plt.xticks([])
+                plt.yticks([])
+
+                plt.imshow(img, cmap=plt.cm.binary)
+
+                predicted_label = np.argmax(predictions_array)
+                if predicted_label == true_label:
+                    color = 'blue'
+                else:
+                    color = 'red'
+
+                plt.xlabel("{} {:2.0f}% ({})".format(names[predicted_label],
+                                            100*np.max(predictions_array),
+                                            names[true_label]),
+                                            color=color)
+
+            def plot_value_array(i, predictions_array, true_label):
+                predictions_array, true_label = predictions_array, true_label[i]
+                plt.grid(False)
+                plt.xticks(range(10))
+                plt.yticks([])
+                thisplot = plt.bar(range(10), predictions_array, color="#777777")
+                plt.ylim([0, 1])
+                predicted_label = np.argmax(predictions_array)
+
+                thisplot[predicted_label].set_color('red')
+                thisplot[true_label].set_color('blue')
+            
+            if give_curve == True:
+                num_rows = 5
+                num_cols = 3
+                num_images = num_rows*num_cols
+                plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+                for i in range(num_images):
+                    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+                    plot_image(i, predictions[i], self.Y_test, self.X_test)
+                    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+                    plot_value_array(i, predictions[i], self.Y_test)
+                plt.tight_layout()
+                plt.show()
+
+            NN_output_args = np.argmax(predictions, axis = 1)
+            real_output_args = self.Y_test
+
+            test_accuracy = tf.keras.metrics.Accuracy()
+            test_accuracy(NN_output_args, real_output_args)
+        
+        else:
+            # set weights and predict with model
+            self.model.set_weights(self.nn_weights)
+            predictions = self.model.predict(self.X_test)
+
+            NN_output_args = np.argmax(predictions, axis = 1)
+            real_output_args = np.argmax(self.Y_test, axis = 1)
+
+            if give_curve == True:
+                plt.figure(figsize = (10,1))
+                plt.plot(NN_output_args,'ro', label = "NN")
+                plt.plot(real_output_args, 'bo', label = "Real")
+                plt.legend()
+
+            test_accuracy = tf.keras.metrics.Accuracy()
+            test_accuracy(NN_output_args, real_output_args)      
+        
+        return test_accuracy.result()
+
+
+    def plot_training(self, figName = None):
         try:
             self.train_curve
         except AttributeError:
             print("No training curve available. Either training hasn't occured, or data recording was suppressed via the \"train_fast\" parameter")
             return
-        plt.plot(self.train_curve[:,0], label="Training")
-        plt.plot(self.train_curve[:,1], label="Validation")
+        plt.plot(self.train_curve[:,0], '-', label="Training")
+        plt.plot(self.train_curve[:,1], '--', label="Validation")
+        plt.xlabel('Epochs')
+        plt.ylabel('MSE')
         plt.legend()
         plt.show()
+        
+        if figName != None:
+            plt.savefig(str(figName), bbox_inches="tight")
 
     def _Convert(self, pre_w):
         position = []
